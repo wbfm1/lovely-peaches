738986e8dc94b982503c670693065b69361e735c
wbfm1 <martinusss111@gmail.com>
2024-11-10 22:07:47 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Optional JS for extra control if you want to add interactive elements later
+document.addEventListener('DOMContentLoaded', function() {
+    const video = document.querySelector('.video');
+    
+    // You can add some JS functionality if you want to interact with the video or text
+});
